0e9a4ec57d788b9db7aa6a8d5bb9e74d0bd65743
Nick Bechtel <bechtel.nick@gmail.com>
2021-10-17 23:59:19 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  const passedDrivers = drivers.filter((element) => element === string);
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === string.toLowerCase()
+  );
+  return passedDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+  const length = string.length;
+  const driverSpecific = drivers.filter(function (element) {
+    return element.slice(0, 2) === string;
+  });
+  return driverSpecific;
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (hometown) {
+    return hometown.name === string;
+  });
+}
